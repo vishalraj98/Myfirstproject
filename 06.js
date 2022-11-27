e5703e0f135f61a10e8b8379caf06fc02ae5f671
vishal <vishal98mi@gmail.com>
2022-11-27 13:18:37 +0530

    my second commit

--- 06.js
+++ 06.js
@@ -1,18 +1,24 @@
 //Guess a number use prompt also use +promt bez they firstly declared string so use +
-let winningno= 20;
-let userguess= +prompt("Guess a number");
+// let winningno= 20;
+// let userguess= +prompt("Guess a number");
 
-// console.log(typeof userguess, userguess);
+// // console.log(typeof userguess, userguess);
 
-if(userguess === winningno)
-{
-    console.log("your guess is right");
-}else{
-    if(userguess < winningno){
-    concole.log("its to low");
-}else
-{
-    concole.log("its to high")
-}
-}
+// if(userguess === winningno)
+// {
+//     console.log("your guess is right");
+// }else{
+//     if(userguess < winningno){
+//     concole.log("its to low");
+// }else
+// {
+//     concole.log("its to high")
+// }
+// }
 
+
+   var  myvar ;
+    console.log(typeof "tty")
+    var myVar = "vishal"
+
+    console.log(myVar)
